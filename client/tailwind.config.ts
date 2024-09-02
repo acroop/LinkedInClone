5a476e5d9f048a46f1c9659bd92afd09708afe64
Supratik Das <dassupratik86@gmail.com>
2024-09-02 12:11:06 +0530

    navbar

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,7 +6,10 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
+    
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -15,6 +18,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
