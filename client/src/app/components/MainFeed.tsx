17511cdf8dd2624be76cb0e7a752a02873cb9ac9
Supratik Das <dassupratik86@gmail.com>
2024-09-02 13:29:54 +0530

    papa bolte

--- client/src/app/components/MainFeed.tsx
+++ client/src/app/components/MainFeed.tsx
@@ -1,15 +1,35 @@
+import Image from 'next/image'
 import React from 'react'
+
 
 const MainFeed = () => {
   return (
-    <div className='w-full'>
-        <div>
-            <div className='w-full flex justify-center p-2'>
-                <div></div>
-                <input type="text" placeholder='Type....' className='w-[75%] h-12 p-2 border-2 rounded-xl' />
-                <div></div>
-            </div>
+    <div className='w-full '>
+      <div className=' border-b-8'>
+        <div className='w-full flex flex-col items-center justify-center p-2 m-3'>
+          <div className=' flex w-full justify-center gap-4'>
+            <Image
+            className=' rounded-full'
+            height={10}
+            width={45}
+            alt='Profile'
+            src="/pp.jpg"
+            />
+            <input type="text" placeholder='Type....' className='w-[75%] h-12 p-2 border-2 rounded-xl' />
+          </div>
+          
+          <div className=' flex justify-evenly w-full items-center m-2 p-2'>
+            <div className='hover:bg-[#ecebeb] cursor-pointer p-4 rounded-md transition duration-500 flex items-center gap-2 '>
+              <img className=' w-8' src="/gal.svg" alt="" />
+              Media</div>
+
+            <div className=' hover:bg-[#ecebeb] cursor-pointer p-4 rounded-md transition duration-500 flex items-center gap-2'>
+              <img className=' w-8' src="/pen.svg" alt='' />
+              Write</div>
+          </div>
         </div>
+
+      </div>
     </div>
   )
 }
--- client/src/app/components/NavbarComp.tsx
+++ client/src/app/components/NavbarComp.tsx
@@ -32,7 +32,7 @@ function NavbarComp() {
                         <div className="w-10 rounded-full">
                             <img
                                 alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                src="/pp.jpg" />
                         </div>
                     </div>
                     <ul
