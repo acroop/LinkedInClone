5b3b9acc8ad98076f60be5e9b0fcf3dd670e090f
Aman <amansinha561@gmail.com>
2024-09-04 20:44:11 +0530

    check

--- client/src/app/components/LeftFeed.tsx
+++ client/src/app/components/LeftFeed.tsx
@@ -1,14 +1,111 @@
-import React from 'react'
+import React from "react";
 
 const LeftFeed = () => {
   return (
-    <div className='flex justify-center items-center m-2 flex-col gap-1'>
-        <img src="./pp.jpg" className='w-[50px] rounded-full' alt="" />
-        <h1 className='font-extrabold'>Supratik Das</h1>
-        <p className='text-[15px] text-[#818080]'>SDE</p>
-        <button className='bg-[#414040] text-[13px] text-white p-2 rounded-2xl'>View Profile</button>
+    <div>
+      <div className="border-b-8 w-full">
+        <div className="flex justify-center items-center m-2 p-2.5 flex-col gap-1">
+          <img src="./pp.jpg" className="w-[50px] rounded-full" alt="" />
+          <h1 className="font-extrabold">Supratik Das</h1>
+          <p className="text-[15px] text-[#818080]">SDE</p>
+          <button className="bg-[#414040] text-[13px] text-white p-2 rounded-2xl">
+            View Profile
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 justify-center items-center p-2">
+        <h1 className="text-[20px] font-bold">You May Know</h1>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Maya Kapoor</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at Zeta</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Aanya Sharma</span>
+            <p className="text-[13px] text-[#818080] text-center">Founder of GoTech</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Rohit Nair</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at InMobi</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Priya Iyer</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at CRED</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Aditya Verma</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at Swiggy</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Neha Singh</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at Dunzo</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Vikas Patil</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at BYJU's</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Sneha Reddy</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at Lenskart</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Arjun Desai</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at Meesho</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Kavya Bhat</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at Paytm</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <img src="./1.png" className="w-[50px] rounded-full" alt="" />
+          <div className="flex flex-col w-[120px]">
+            <span className="text-center">Rohan Khanna</span>
+            <p className="text-[13px] text-[#818080] text-center">Works at Zomato</p>
+          </div>
+          <img src="./add.png" className="w-[25px]" alt="Add" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeftFeed
+export default LeftFeed;
